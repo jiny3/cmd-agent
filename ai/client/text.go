ac7e1feb293bcb352228502a10c7a2d38dd2ff71
jiny3 <jinyan1419@gmail.com>
2025-05-25 18:58:27 +0800

    🎈 perf: update system prompt

--- ai/client/text.go
+++ ai/client/text.go
@@ -9,7 +9,7 @@ var systemContent = &genai.Content{
 	Parts: []*genai.Part{
 		{
 			// A system prompt to instruct the AI model about the task
-			Text: "You are a shell expert, please help me complete the following command, you should output the completed command, no need to include any other explanation. Do not put completed command in a code block. Then execute the command by cmd_executor function call.",
+			Text: "You are a shell expert, please help me complete the following command and set the appropriate timeout, you should output the completed command, no need to include any other explanation. Do not put completed command in a code block. Then execute the command by cmd_executor function call.",
 		},
 	},
 }
