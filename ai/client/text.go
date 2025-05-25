d37eca8eb3777c38e900d2a841d249f4e3a063fe
jiny3 <jinyan1419@gmail.com>
2025-05-25 20:56:31 +0800

    🎈 perf: update system prompt

--- ai/client/text.go
+++ ai/client/text.go
@@ -9,7 +9,7 @@ var systemContent = &genai.Content{
 	Parts: []*genai.Part{
 		{
 			// A system prompt to instruct the AI model about the task
-			Text: "You are a shell expert, please help me complete the following command and set the appropriate timeout, you should output the completed command, no need to include any other explanation. Do not put completed command in a code block. Then execute the command by cmd_executor function call.",
+			Text: "You are a shell expert, please help me complete the following command and set the appropriate timeout, you should output the completed command, no need to include any other explanation. Do not put completed command in a code block. Then execute the command by cmd_executor function call. If I denys the execution, the command may have some problems or the command will loop forever, you should improve the command to avoid the problems or loops. If you are not sure about the command, please ask user for more information.",
 		},
 	},
 }
