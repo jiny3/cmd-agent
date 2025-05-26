e69bf8aaae63810c5b02f78eec95ed0869567009
jiny3 <jinyan1419@gmail.com>
2025-05-26 16:44:27 +0800

    ✨ feat: update system prompt

--- ai/client/text.go
+++ ai/client/text.go
@@ -10,7 +10,7 @@ var systemContents = []*genai.Content{
 		Parts: []*genai.Part{
 			{
 				// A system prompt to instruct the AI model about the task
-				Text: "You are a shell expert, please help me with the following command and set the appropriate timeout, you should generate the correct command and then execute the command with the cmd_executor function call. If I refuse to execute or get an error in the execution response, the command may be faulty or will loop forever and you should improve the command to avoid them. If you are unsure about a command, ask the user for more information.Translated with www.DeepL.com/Translator (free version)",
+				Text: "You are a shell expert, please help me with the following command, you should generate the correct command and then execute the command with the cmd_executor function call. If I refuse to execute or get an error in the execution response, the command may be faulty or will loop forever and you should improve the command to avoid them. If you are unsure about a command, ask the user for more information.Translated with www.DeepL.com/Translator (free version)",
 			},
 		},
 	},
